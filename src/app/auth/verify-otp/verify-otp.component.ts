90dae1fe88eed432c2fcbe4ab2602646cfe89c38
jaakumasi <jerome.akumasi@amalitech.com>
2024-05-21 16:03:06 +0000

    feat(auth): verify otp markup

--- src/app/auth/verify-otp/verify-otp.component.ts
+++ src/app/auth/verify-otp/verify-otp.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { MessageBoxComponent } from '../../_shared/components/message-box/message-box.component';
+import { REDUCERS } from '../../_shared/constants';
+import { ActionBtnComponent } from '../_shared/components/action-btn/action-btn.component';
+import { GrogHeaderComponent } from '../_shared/components/grog-header/grog-header.component';
 
 @Component({
   selector: 'app-verify-otp',
   standalone: true,
-  imports: [],
+  imports: [
+    ActionBtnComponent,
+    CommonModule,
+    FormsModule,
+    GrogHeaderComponent,
+    MessageBoxComponent,
+  ],
   templateUrl: './verify-otp.component.html',
-  styleUrl: './verify-otp.component.scss'
+  styleUrl: './verify-otp.component.scss',
 })
-export class VerifyOtpComponent {
+export class VerifyOtpComponent implements OnInit {
+  state$ = inject(Store).select(REDUCERS.GLOBAL_STATE_REDUCER);
+  isSmall = false;
+  otpInput1 = '';
+  otpInput2 = '';
+  otpInput3 = '';
+  inputRegex = /\d/
 
+   ngOnInit(): void {
+    this.state$.subscribe((screenSize) => {
+      const screen = screenSize.screenSize;
+      this.isSmall = screen === 'xsmall' || screen === 'small';
+    });
+  }
+
+  onOtpChange(value: string, ref: HTMLInputElement) {
+    const matches = this.inputRegex.test(value);
+    if(matches) ref.focus()
+  }
 }
