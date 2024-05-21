d7c20e9d89d80c407c2854350b7a2384d5a636f8
jaakumasi <jerome.akumasi@amalitech.com>
2024-05-21 14:57:09 +0000

    feat(auth): request otp markup

--- src/app/auth/request-otp/request-otp.component.ts
+++ src/app/auth/request-otp/request-otp.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { MessageBoxComponent } from '../../_shared/components/message-box/message-box.component';
+import { REDUCERS } from '../../_shared/constants';
+import { FormControlComponent } from '../_shared/components/form-control/form-control.component';
+import { GrogHeaderComponent } from '../_shared/components/grog-header/grog-header.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { emailValidator } from '../_shared/validators/email.validator';
+import { ActionBtnComponent } from '../_shared/components/action-btn/action-btn.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-request-otp',
   standalone: true,
-  imports: [],
+  imports: [
+    ActionBtnComponent,
+    CommonModule,
+    FormControlComponent,
+    GrogHeaderComponent,
+    MessageBoxComponent,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
   templateUrl: './request-otp.component.html',
-  styleUrl: './request-otp.component.scss'
+  styleUrl: './request-otp.component.scss',
 })
-export class RequestOtpComponent {
+export class RequestOtpComponent implements OnInit {
+  state$ = inject(Store).select(REDUCERS.GLOBAL_STATE_REDUCER);
+  isSmall = false;
+  formBuiilder = inject(FormBuilder);
+  requestOtpForm!: FormGroup;
 
+  ngOnInit(): void {
+    this.state$.subscribe((screenSize) => {
+      const screen = screenSize.screenSize;
+      this.isSmall = screen == 'small' || screen === 'xsmall';
+    });
+
+    this.requestOtpForm = this.formBuiilder.group({
+      email: ['', [Validators.required, emailValidator]],
+    });
+  }
+
+  get emailRequired() {
+    return (
+      this.requestOtpForm.get('email')?.touched &&
+      this.requestOtpForm.get('email')?.hasError('email')
+    );
+  }
 }
